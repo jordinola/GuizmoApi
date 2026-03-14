010b2c8e8161a6c7c6ef2788ecd55c76a8791bd3
James Ordinola <james.ordinola@navix.io>
2026-03-14 10:14:38 -0500

    feat(domain): add Guizmo entity POCO

---
+++ src/GuizmoApi.Domain/Entities/Guizmo.cs
@@ -0,0 +1,10 @@
+namespace GuizmoApi.Domain.Entities;
+
+public class Guizmo
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Manufacturer { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public decimal Msrp { get; set; }
+}
